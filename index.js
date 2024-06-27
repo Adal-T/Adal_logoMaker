943c3c9c7d8ad6aac5dc0f373cad6ee163516975
Adal <tsegai.adal.2020@gmail.com>
2024-06-27 01:26:34 -0700

    added index.js file

---
+++ index.js
@@ -0,0 +1,50 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const {Square, Circle, Triangle} = require('./shape')
+
+
+const questions = [
+    {
+        name: "logoTitle",
+        message: "what is your logo name?",
+        type: "input"
+    },
+    {
+        name: "logoTextColor",
+        message: "what is your text color?",
+        type: "input"
+    },
+    {
+        name: "logoShapeColor",
+        message: "what is your logo color?",
+        type: "input"
+    },
+    {
+        name: "logoShape",
+        message: "what is your logo shape?",
+        type: "list",
+        choices: [
+            "Circle",
+            "Square",
+            "Triangle",
+        ]
+    }
+];
+
+function init() {
+    inquirer.prompt(questions).then(data =>{
+        let shape;
+        if (data.logoShape === "Circle"){
+            shape = new Circle(data.logoTitle, data.logoTextColor, data.logoShapeColor)
+        }
+        else if (data.logoShape === "Square"){
+            shape = new Square(data.logoTitle, data.logoTextColor, data.logoShapeColor)
+        }
+        else if (data.logoShape === "Triangle"){
+            shape = new Triangle(data.logoTitle, data.logoTextColor, data.logoShapeColor)
+        }
+        fs.writeFileSync("./logo.svg", shape.render())
+    })
+}
+
+init();
